0f94a235603c91dd51b6525369b9514bd9043192
danielsantosf <daniel.santos@4yousee.com.br>
2019-07-10 18:06:11 -0300

    First tests written thus far

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,60 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists} from '../src/main';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+chai.use(sinonChai);
+sinonStubPromise(sinon);
 
-describe('Main', () => {
+//enable global fetch within our interface
+global.fetch = require('node-fetch');
 
+describe('Spotify Wrapper', () => {
+
+  describe('smoke tests', () => {
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+
+  });
+
+  describe('Generic Search', () =>{
+
+    it('should call fetch function', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
+
+      fetchedStub.restore();
+    });
+
+    it('should receive the correct url to fetch', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search('Madonna', 'artist');
+
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Madonna&type=artist');
+
+      const albums = search('Madonna', 'album');
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Madonna&type=album');
+
+    });
+  });
 });
