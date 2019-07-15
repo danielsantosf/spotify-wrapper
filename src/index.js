bad647326ee6e0363848ee4b79b5fb507ee3c176
danielsantosf <daniel.santos@4yousee.com.br>
2019-07-15 11:19:14 -0300

    Fixed lint warnings/errors

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import {
   searchArtists,
   searchAlbums,
   searchTracks,
-  searchPlaylists
+  searchPlaylists,
 } from './main';
 
 import {
   getAlbum,
   getAlbums,
-  getAlbumTracks
+  getAlbumTracks,
 } from './album';
 
 export {
@@ -20,5 +20,5 @@ export {
   searchPlaylists,
   getAlbum,
   getAlbums,
-  getAlbumTracks
-}
+  getAlbumTracks,
+};
--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
+/* global fetch */
 import { API_URL, HEADERS } from './config';
 import { toJSON } from './utils';
 
 export const search = (query, type) =>
-  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`, HEADERS)
-    .then(toJSON)
+  fetch(`${API_URL}/search?q=${query}&type=${type}`, HEADERS)
+    .then(toJSON);
 
-export const searchArtists = (query) =>
+export const searchArtists = query =>
   search(query, 'artist');
 
-export const searchAlbums = (query) =>
+export const searchAlbums = query =>
   search(query, 'album');
 
-export const searchTracks = (query) =>
+export const searchTracks = query =>
   search(query, 'track');
 
-export const searchPlaylists = (query) =>
+export const searchPlaylists = query =>
   search(query, 'playlist');
